7826287ecf8f846933498bd62e034c93fc6f0294
Maria Lobillo-Santos <161346440+mariaremexp@users.noreply.github.com>
2024-04-08 12:22:06 +0200

    feat: add scroll to the bottom

--- src/components/ChatMessages.jsx
+++ src/components/ChatMessages.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
 const ChatMessages = ({ messages }) => {
+  
+  const listRef = useRef(null);
+
+  useEffect(() => {      
+    listRef.current?.lastElementChild?.scrollIntoView()
+  }, [messages]);
+
   return (
-    <div className="fixed left-0 w-full h-4/7 max-h-48 text-clip overflow-hidden bottom-[65px] p-4 
-          bg-white opacity-80 mx-auto container">
-        <div className="imessage">
+    <div className="fixed left-0 w-full h-4/6 max-h-96 text-clip bottom-[65px] p-4 
+          bg-white opacity-80 mx-auto container overflow-auto" 
+          style={{ maxHeight: "400px" }}
+        >
+        <div className="imessage " ref={listRef} >
           {messages && messages.map((message, index) => 
             <p key={index} className={message.sender}>
               {message.content}
             </p>  
           )}
-        </div>     
+        </div>
     </div>
   )
 }
