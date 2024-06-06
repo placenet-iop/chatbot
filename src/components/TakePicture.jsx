147f1e4ba8b55229c3bbb39d9aa407439aa1e376
Maria Lobillo-Santos <161346440+mariaremexp@users.noreply.github.com>
2024-06-06 17:09:10 +0200

    feat: add component to take picture for ecogirl

--- src/components/TakePicture.jsx
+++ src/components/TakePicture.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { div } from 'three/examples/jsm/nodes/Nodes.js'
 
 const TakePicture = () => {
+    const webcamRef = useRef(null)
+    const [imageSrc, setImageSrc] = useState(null)
+
+    const capture = () => {
+        const imageSrc = webcamRef.current.getScreenshot()
+        setImageSrc(imageSrc)
+    }
   return (
-    <div>
-      
-    </div>
+    <>
+      <Webcam 
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={320}
+        height={240}
+      />
+      <button onClick={capture}> Capture Picture</button>
+      {imageSrc && (
+        <div>
+            <h3>Captured Image: </h3>
+            <img src={imageSrc} alt="Captured" />
+        </div>
+      )}
+    </>
   )
 }
 
